57a98b66a7a2e822324599c9e71705934884148b
htamlive <nghichmaychoi@gmail.com>
2025-04-04 09:34:09 +0700

    add agent benchmarking

---
+++ run_benchmark.sh
@@ -0,0 +1,7 @@
+python run_benchmark.py \
+    --agent1 F:/Cliquey/DTAI-Judge/examples/agents/bot1/main.py \
+    --agent2 F:/Cliquey/DTAI-Judge/examples/agents/bot2/main.py \
+    --agent3 F:/Cliquey/DTAI-Judge/examples/agents/bot11/main.py \
+    --map_path F:/Cliquey/DTAI-Judge/examples/maps/map.json \
+    --n_rounds 10 \
+    --max_workers 8
