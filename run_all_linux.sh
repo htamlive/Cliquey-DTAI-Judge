7c0ec5ccc0838ee35cc303915739ae92e660f060
nhphucqt <hoangphuc.qti@gmail.com>
2025-04-05 08:22:28 +0700

    create run_all_linux.sh

--- run_all_linux.sh
+++ run_all_linux.sh
@@ -1,17 +1,18 @@
-EX_AGENT_DIR=examples/agents
-AGENT_DIR=../agents
 MAP_DIR=examples/maps
 
-MAP_NAME=map5
-N_ROUNDS=10
+MAP_NAME=$1
+N_ROUNDS=$2
 MAX_WORKERS=20
 
-AGENT_1_NAME=bot4
-AGENT_1_FILE=$EX_AGENT_DIR/bot4/main.py
-AGENT_2_NAME=phuc_old
-AGENT_2_FILE=$AGENT_DIR/phuc_old/phuc_old.py
-AGENT_3_NAME=Cliquey
-AGENT_3_FILE=../out/Cliquey/main
+AGENT_1_FILE=$3
+AGENT_2_FILE=$4
+AGENT_3_FILE=$5
+
+AGENT_1_NAME=$(basename $(dirname $AGENT_1_FILE))
+AGENT_2_NAME=$(basename $(dirname $AGENT_2_FILE))
+AGENT_3_NAME=$(basename $(dirname $AGENT_3_FILE))
+
+echo "Running benchmark with agents: $AGENT_1_NAME (bot1), $AGENT_2_NAME (bot2), $AGENT_3_NAME (bot3) on map: $MAP_NAME"
 
 MAP_PATH=$MAP_DIR/$MAP_NAME.json
 MATCH_LOG_DIR=data/logs/$AGENT_1_NAME\_vs\_$AGENT_2_NAME\_vs\_$AGENT_3_NAME\_vs\_$MAP_NAME
